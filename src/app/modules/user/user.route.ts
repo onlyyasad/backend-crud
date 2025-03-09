3f081be663507a1360a8be95009ef49707632cd6
hafij <hafij.alasad23@gmail.com>
2025-03-10 02:34:39 +0600

    feat: api routes created for all controllers

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,15 @@ import { UserControllers } from './user.controller'
 const router = express.Router()
 
 router.get('/', UserControllers.getUsers)
+router.post('/', UserControllers.createUser)
+router.get('/:userId', UserControllers.getUserById)
+router.put('/:userId', UserControllers.updateUser)
+router.delete('/:userId', UserControllers.deleteUser)
+router.put('/:userId/orders', UserControllers.addProductToUserOrders)
+router.get('/:userId/orders', UserControllers.getUserOrders)
+router.get(
+  '/:userId/orders/total-price',
+  UserControllers.getTotalPriceOfUserOrders,
+)
 
 export const UserRoutes = router

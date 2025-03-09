b7897facb16e4816123b1f3be3d7bc373a511b97
hafij <hafij.alasad23@gmail.com>
2025-03-09 23:30:34 +0600

    feat: user schema refined

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,27 +1,85 @@
 import mongoose from 'mongoose'
 
+const fullNameSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required.'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+})
+
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: [true, 'Street is required.'],
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required.'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required.'],
+  },
+})
+
+const ordersSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: [true, 'Product name is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required.'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required.'],
+  },
+})
+
 const userSchema = new mongoose.Schema({
-  userId: Number,
-  username: String,
-  password: String,
+  userId: {
+    type: Number,
+    required: [true, 'User id is required.'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'username is Required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is Required'],
+  },
   fullName: {
-    firstName: String,
-    lastName: String,
+    type: fullNameSchema,
+    required: [true, 'Full name is required'],
   },
-  age: Number,
-  email: String,
-  isActive: Boolean,
-  hobbies: [String],
+  age: {
+    type: Number,
+    required: [true, 'Age is required.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobbies are required.'],
+  },
   address: {
-    street: String,
-    city: String,
-    country: String,
+    type: addressSchema,
+    required: [true, 'Address is required.'],
   },
-  orders: {
-    productName: String,
-    price: String,
-    quantity: Number,
-  },
+  orders: ordersSchema,
 })
 
 export const User = mongoose.model('User', userSchema)
